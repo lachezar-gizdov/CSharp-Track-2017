b46f3de0689031845260fe8e1b4e050cfaddfbea
olebg <olebg01@gmail.com>
2016-11-04 00:49:19 +0200

    homeworks

--- CSharp-1/06.Loops/14.HexToDecimal/HexToDecimal.cs
+++ CSharp-1/06.Loops/14.HexToDecimal/HexToDecimal.cs
@@ -6,8 +6,41 @@ class DecimalToHex
     {
         {
             string input = Console.ReadLine();
+            long temp = 0;
+            double result = 0;
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                switch (arr[i])
+                {
+                    case 'A':
+                        temp = 10;
+                        break;
+                    case 'B':
+                        temp = 11;
+                        break;
+                    case 'C':
+                        temp = 12;
+                        break;
+                    case 'D':
+                        temp = 13;
+                        break;
+                    case 'E':
+                        temp = 14;
+                        break;
+                    case 'F':
+                        temp = 15;
+                        break;
+                    default:
+                        temp = arr[i] - '0';
+                        break;
+                }
 
+                result += temp * Math.Pow(16, i);
+            }
+            Console.WriteLine(result.ToString());
         }
     }
 }
