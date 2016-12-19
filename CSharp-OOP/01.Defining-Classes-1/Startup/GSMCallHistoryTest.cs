8e5a7b20e527c5e9797120d5bef2169b7dd8501f
olebg <olebg01@gmail.com>
2016-12-20 01:56:04 +0200

    slow progress

--- CSharp-OOP/01.Defining-Classes-1/Startup/GSMCallHistoryTest.cs
+++ CSharp-OOP/01.Defining-Classes-1/Startup/GSMCallHistoryTest.cs
@@ -8,13 +8,10 @@ namespace Startup
         {
             GSM Nexus = new GSM("LG", "Nexus", 600, "Lucho", new Battery(10, 15, BatteryType.LiIon));
 
-            List<Call> calls = new List<Call>();
-
             Nexus.AddCall(new Call("18.12.2016", "15:30", 1111345, 60));
             Nexus.AddCall(new Call("19.12.2016", "17:30", 3333345, 120));
             Nexus.AddCall(new Call("20.12.2016", "20:00", 7777345, 200));
 
-            Nexus.DisplayCallHistory(calls);
         }
     }
 }
