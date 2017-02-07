58fb9991a9076f3ea0aabcd664d21231e0c4d0f9
olebg <olebg01@gmail.com>
2017-02-07 15:02:17 +0200

    moq mocks

--- CSharp-Unit-Testing/Homeworks/03.Cars-Example/Cars.Tests.JustMock/Mocks/MoqCarsRepository.cs
+++ CSharp-Unit-Testing/Homeworks/03.Cars-Example/Cars.Tests.JustMock/Mocks/MoqCarsRepository.cs
@@ -14,6 +14,8 @@
             mockedCarsRepository.Setup(r => r.All()).Returns(this.FakeCarCollection);
             mockedCarsRepository.Setup(r => r.Search(It.IsAny<string>())).Returns(this.FakeCarCollection.Where(c => c.Make == "BMW").ToList());
             mockedCarsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(this.FakeCarCollection.First());
+            mockedCarsRepository.Setup(r => r.SortedByMake()).Returns(this.FakeCarCollection.OrderBy(car => car.Make).ToList);
+            mockedCarsRepository.Setup(r => r.SortedByYear()).Returns(this.FakeCarCollection.OrderBy(car => car.Year).ToList);
             this.CarsData = mockedCarsRepository.Object;
         }
     }
