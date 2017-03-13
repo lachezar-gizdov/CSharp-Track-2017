30b43986b4df914498a26be45e472b4136859eef
Lucho Gizdov <olebg01@gmail.com>
2017-03-13 23:34:02 +0200

    homeworks progress

---
+++ High-Quality-Code-1/03.Naming-Identifiers/03.ThirdTask/app.js
@@ -0,0 +1,10 @@
+function _ClickON_TheButton(THE_event, args) {
+    var myWindow = window, browser = myWindow.navigator.appCodeName, ism = browser == "Mozilla";
+    if (ism) {
+        alert("Yes");
+    }
+    else {
+        alert("No");
+    }
+}
+//# sourceMappingURL=app.js.map
