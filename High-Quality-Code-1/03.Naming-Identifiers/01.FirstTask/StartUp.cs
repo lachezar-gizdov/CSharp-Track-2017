2251593c7df7953486d9ad2ce8c5c7f2139ff5f8
Lucho Gizdov <olebg01@gmail.com>
2017-03-13 23:43:44 +0200

    HQC-Part1 homeworks

---
+++ High-Quality-Code-1/03.Naming-Identifiers/01.FirstTask/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstTask
+{
+    using Engine;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Variable variable = new Variable();
+            variable.Print(true);
+        }
+    }
+}
