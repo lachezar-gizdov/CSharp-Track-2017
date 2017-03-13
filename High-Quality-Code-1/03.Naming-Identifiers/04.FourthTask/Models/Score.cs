549200644ccaa165866a714cf8fc8d9b4953bca5
Lucho Gizdov <olebg01@gmail.com>
2017-03-13 23:50:59 +0200

    progress on fourth task

---
+++ High-Quality-Code-1/03.Naming-Identifiers/04.FourthTask/Models/Score.cs
@@ -0,0 +1,40 @@
+﻿namespace Minesweeper.Models
+{
+    public class Score
+    {
+        private string name;
+        private int points;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                points = value;
+            }
+        }
+
+        public Score() { }
+
+        public Score(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+    }
+}
