76c049c392bf652df1ccd7c25ec285ff25a0b6f1
Lucho Gizdov <olebg01@gmail.com>
2017-05-03 14:48:31 +0300

    started solving workshop from 28.4.2017

---
+++ High-Quality-Code-2/08.Workshops/ConsoleApplication3 - 28-04-2017/ConsoleApplication3/Mark.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApplication3
+{
+    public class Mark {
+        private float value;
+        private Subject subject;
+
+        public Mark(Subject sbj, float va) {
+            subject = sbj;
+            value = va;
+        }
+
+        public float Value { get; set; }
+
+        public Subject Subject { get; set; }
+    }
+}
---
+++ High-Quality-Code-2/08.Workshops/ConsoleApplication3 - 28-04-2017/ConsoleApplication3/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApplication3
+{
+    using System.Linq;
+    using System.Collections.Generic;
+    public class Student
+    {
+        private string firstName;
+        private Grade grades;
+        private List<Mark> mark;
+        private string lastName;
+
+        public Student(string _, string __, Grade ___)
+        {
+            firstName = _;
+            lastName = __;
+            grades = ___;
+            mark = new List<Mark>();
+        }
+
+        public List<Mark> Mark { get; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Grade Grade { get; set; }
+
+        public string ListMarks()
+        {
+            var potatos = mark.Select(m => $"{m.Subject} => {m.Value}").ToList();
+            return string.Join("\n", potatos);
+        }
+    }
+}
